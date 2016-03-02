6c78335d46aed949c6d33e5000a74b58c7e327f1
Nikita Rai <nikita.collier@gmail.com>
2016-03-02 10:43:11 -0500

    new file changes

---
+++ gitpractice2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitpractice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 8;
+            int b = 10;
+            int c = b - a;
+            Console.WriteLine(c);
+            
+        }
+    }
+}
